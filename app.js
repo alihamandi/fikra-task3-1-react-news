ff294d7fc8aea98a91034b9d200cdde059d2fd70
Ali Y. Hamandi <aliyasiralasady@hotmail.com>
2018-11-19 19:15:26 +0300

    with out style now

--- app.js
+++ app.js
@@ -1,54 +1,112 @@
 import styled from 'styled-components';
-import React,{Components} from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import logo from './assets/logo.png' ;
 import magn from './assets/mag.png'
 
 
+class News extends Component{
+    constructor(){
+        super()
+        this.state = {
+            news : []
+        }
+        fetch('https://newsapi.org/v2/everything?q=bitcoin&apiKey=1304dda50b3c4066b642db00ccf7ae98')
+        .then((response)=>{
+            return response.json()
+        }).then((data)=>{
+            this.setState({
+                news: data.articles
+            })
+            
+        })
+    }
 
-
-
-function Navbar(){
-    return <header id="navBar">
-        <div >
-            <img id="logo" src={logo} alt="logo"/>
-        </div>
-        <div id="search">
-            <div id="search-box">
+    render(){
+        return <React.Fragment>
+            <header id="navBar">
                 <div >
-                    <img id="magn" src={magn} alt=""/>  
+                    <img id="logo" src={logo} alt="logo"/>
                 </div>
-                <input id="search" placeholder="Search a topic" type="text"/>
+                <div id="search">
+                    <div id="search-box">
+                        <div >
+                            <img id="magn" src={magn} alt=""/>  
+                        </div>
+                        <input id="search" placeholder="Search a topic" type="text"/>
+                    </div>
+                </div>
+            </header>
+    
+            <div>
+            
+            {
+                this.state.news.map((item , i)=>{
+                
+                    return(
+                    <div key={i} id="main">
+                        <article  className="article">
+                            <div  className="photo">
+                                <img width="50%" height="100%"  src={item.urlToImage} alt="photo"/>
+                            </div>
+                            <div className="content">
+                                <div className="head">
+                                    <h1>{item.title}</h1>
+                                </div>
+                                <div className="des">
+                                    <p>{item.description}</p>
+                                </div>
+                                <div className="date">
+                                    <time>{item.publishedAt}</time>
+                                </div>
+                            </div>
+                        </article>
+                    </div>)
+                    
+
+            })
+            
+            }
+
             </div>
-        </div>
-    </header>
+        </React.Fragment>
+        
+    }
 }
 
-function Body(){
-    return <article>
-        <div className="photo">
-            <img src="https://unsplash.it/200/200" alt="photo"/>
-        </div>
-        <div className="head">
-            <h1>Lorem ipsum</h1>
-        </div>
-        <div className="des">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit dolor earum quod deserunt ullam eos repellat animi cum modi illo, esse quam, tempora ducimus nostrum totam maiores. Saepe, cupiditate aliquid.</p>
-        </div>
-        <div className="date">
-            <time>19.10.2018</time>
-        </div>
-    </article>
 
-}
+// function NewsItem(){
+//     return <div id="main">
+//         <article className="article">
+//         <div className="photo">
+//             <img src="https://unsplash.it/200/200" alt="photo"/>
+//         </div>
+//         <div className="content">
+//             <div className="head">
+//                 <h1>Lorem ipsum</h1>
+//             </div>
+//             <div className="des">
+//                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit dolor earum quod deserunt ullam eos repellat animi cum modi illo, esse quam, tempora ducimus nostrum totam maiores. Saepe, cupiditate aliquid.</p>
+//             </div>
+//             <div className="date">
+//                 <time>19.10.2018</time>
+//             </div>
+//         </div>
+        
+//         </article>
+        
+
+//     </div>
+     
+
+// }
 
 
 
 
 function App(){
     return <div>
-        <Navbar/>
-        <Body/>
+        <News/>
     </div>
 }
 
